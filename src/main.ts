0adf9074efdc8d2a6ae1d6a71ebad3dd4624b54f
junaid <junaid1460@gmail.com>
2017-11-03 22:18:49 +0530

    Update : add initial code

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
+// write code to implement network with sapien library
+import { Dense } from '@sapien/layers';
+import { Tensor } from '@sapien/types';
+var t = new Dense()
+var x = new Tensor()
 
-console.log("SapienML loaded : <category>")
+
